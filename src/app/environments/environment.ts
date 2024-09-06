28607e79d06c17737c0185327017b065172cc8a2
mmarquez <mateomarquezdev@gmail.com>
2024-09-06 16:22:42 -0300

    feat(Ambientes): :memo: Se agrega carpeta para manejar ambientes

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    endpoint: 'https://localhost:44329/api',
+};
--- src/app/services/venta.service.ts
+++ src/app/services/venta.service.ts
@@ -3,26 +3,27 @@ import { HttpClient } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
 import { Articulo } from '../models/Articulo';
 import { Vendedor } from '../models/Vendedor';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VentaService {
-  private apiLocal = 'https://localhost:44329/'; // api local
+  private apiLocal = environment.endpoint; // api local
 
   constructor(private http: HttpClient) { }
 
   obtenerArticulos(): Observable<Articulo[]> {
-    return this.http.get<Articulo[]>(`${this.apiLocal}api/Articulos/listarArticulos`)
+    return this.http.get<Articulo[]>(`${this.apiLocal}/Articulos/listarArticulos`)
     
   }
 
   obtenerVendedores(): Observable<Vendedor[]> {
-    return this.http.get<Vendedor[]>(`${this.apiLocal}api/Vendedores/listarVendedores`);
+    return this.http.get<Vendedor[]>(`${this.apiLocal}/Vendedores/listarVendedores`);
   }
 
   guardarPedido(pedido: any): Observable<{ status: string }> {
-    return this.http.post<{ status: string }>(`${this.apiLocal}api/Articulos/validarProducto`, pedido);
+    return this.http.post<{ status: string }>(`${this.apiLocal}/Articulos/validarProducto`, pedido);
   }
   
 }
